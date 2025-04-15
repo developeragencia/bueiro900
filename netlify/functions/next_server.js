3eefc5841d0ab60b97b3cbf9c138bc5662104acb
developeragencia <agenciadeveloper01@gmail.com>
2025-04-15 16:06:14 -0300

    fix: Update Netlify function configuration and dependencies

--- netlify/functions/next_server.js
+++ netlify/functions/next_server.js
@@ -1,20 +1,5 @@
-const { builder } = require('@netlify/functions');
+import { createRequestHandler } from '@netlify/next';
 
-const handler = async (event, context) => {
-  const { next } = require('next');
-  
-  const app = next({
-    dev: false,
-    conf: {
-      distDir: '.next',
-    },
-  });
-
-  await app.prepare();
-  const handle = app.getRequestHandler();
-
-  const response = await handle(event);
-  return response;
-};
-
-exports.handler = builder(handler); 
+export const handler = createRequestHandler({
+  compression: true,
+}); 
